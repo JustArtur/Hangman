65e733ecd06245f6940c8220bdc9635d7f9f999b
Artur Kharisov <93016855+JustArtur@users.noreply.github.com>
2022-01-10 07:55:16 +0600

    Add files via upload

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -1,70 +1,121 @@
-require_relative 'game_bot'
-require_relative 'human'
-
-def check_win(encrypted_word, number_of_mistakes)
-  if number_of_mistakes >= 6
-    puts "Hangman won!"
-    return true
+class Hangman
+  def initialize
+    @lives = 7
+    @wrong_guesses = []
   end
-  unless encrypted_word.include?("_")
-    print "Host win!"
-    return true
+
+  def choose_player
+    puts "Do you want to play with bot y/n?"
+    answer = gets.chomp
+
+    if answer == "y"
+      puts "Game with bot started..."
+      get_secret_word_bot
+    elsif answer == "n"
+      input_secret_word
+    else
+      raise "Please, restart the game"
+    end
   end
-end
 
-def start_game
-  puts "Hangman initialized!"
-  puts "Do you want to play with bot y/n?"
-  answer = gets.chomp
-
-  if answer == "y"
-    puts "Game with bot started..."
-    return get_random_word
-  elsif answer == "n"
-    return input_word
-  else
-    raise "Please, restart the game"
-  end
-end
-
-def input_letter(word, used_letters, encrypted_word, number_of_mistakes)
-  print "Enter letter: "
-  char = gets.chomp.downcase
-  if word.include?(char)
+  def get_secret_word_bot
+    words = File.open("words.txt", "r")
     begin
-      char_index = word.index(char)
-      encrypted_word[char_index] = char
-      word[char_index] = "_"
-      word.include?(char) ? raise("error") : nil
+      secret_word = words.readlines.sample
+      secret_word.length < 5 ? raise("Wrong word") : nil
     rescue
       retry
     end
-  else
-    number_of_mistakes += 1
-    used_letters.append(char)
+    @secret_word = secret_word.chomp.downcase.split("")
+  end
+
+  def input_secret_word
+    print "Please, enter a secret word: "
+    @secret_word = gets.chomp.downcase.split("")
+  end
+
+  def setup_board
+    @board = Array.new(@secret_word.size, "_")
+    @board[0] = @secret_word.first
+    @board[@board.length] = @secret_word.last
+  end
+
+  def make_guess
+    gets.chomp.downcase
+  end
+
+  def start_game
+    # Выбрать игрока
+    # Получить на вход букву
+    # Проверить букву в секретном слове
+    # Проверить победу или поражение
+    puts "Hangman game initialize..."
+    choose_player
+    setup_board
+    output_board
+    begin
+      print "Please, make guess: "
+      @guess = make_guess
+      check_input
+      update_board
+      output_wrong_guesses_and_lives
+      output_board
+      check_win
+    rescue
+      retry
+    end
+  end
+
+  def check_win
+    if not(@board.include?("_"))
+      puts "Hangman won!"
+    elsif not(@lives > 0)
+      puts "Host win!"
+    else
+      raise "Next move"
+    end
+  end
+
+  def check_input
+    if @guess.length > 1
+      puts "You need to enter one letter."
+      output_wrong_guesses_and_lives
+      output_board
+      raise "Wrong input"
+    elsif @wrong_guesses.include?(@guess) or @board.include?(@guess)
+      puts "This letter has already been used."
+      output_wrong_guesses_and_lives
+      output_board
+      raise "Wrong input"
+    end
+  end
+
+  def update_board
+    if @secret_word.include?(@guess)
+      begin
+        guess_index = @secret_word.index(@guess)
+        @board[guess_index] = @guess
+        @secret_word[guess_index] = "_"
+        @secret_word.include?(@guess) ? raise("error") : nil
+      rescue
+        retry
+      end
+    else
+      @lives -= 1
+      puts "Wrong guess("
+      @wrong_guesses.append(@guess)
+    end
+  end
+
+  def output_board
+    print @board.join(""), "\n"
+  end
+
+  def output_wrong_guesses_and_lives
+    puts "Your have #{@lives} lives."
+    puts "Wrong guesses: #{@wrong_guesses.join(",")}."
   end
 end
 
-def create_encrypted_word(word)
-  encrypted_word = Array.new(word.length, "_")
-  encrypted_word[encrypted_word.length - 1] = word.last
-  encrypted_word[0] = word.first
-  word[0] = "_"
-  word[word.length - 1] = "_"
-  return encrypted_word
-end
-
-number_of_mistakes = 0
-used_letters = []
-
-word = start_game
-encrypted_word = create_encrypted_word(word)
-
-while true
-  system "cls"
-  print encrypted_word.join, "\n"
-  input_letter(word, used_letters, encrypted_word, number_of_mistakes)
-  print "Invalid characters: #{used_letters}", "\n"
-  print "Number of mistakes: #{number_of_mistakes}.", "\n"
-  check_win(encrypted_word, number_of_mistakes) ? break : nil
-end
+game = Hangman.new
+game.start_game
